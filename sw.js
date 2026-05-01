63f193139318842728a179cdd0e7a96d3065d1f4
SchEAx <enesagar.s3n3@gmail.com>
2026-05-01 21:25:41 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const CACHE_NAME = "stok-takip-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match("./index.html")))
-  );
-});
+const CACHE_NAME = "stok-takip-v3";
+const ASSETS = ["./", "./index.html", "./style.css", "./app.js", "./manifest.webmanifest"];
+self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
+self.addEventListener("fetch", (event) => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match("./index.html")))); });
